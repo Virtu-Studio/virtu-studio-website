3e20c9a218b9a3592bff4c228786030643ef9c61
Rob Martin <rmartin3@alumni.unca.edu>
2022-05-23 23:14:08 -0400

    Add github Actions

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Virtu Studio</h1>
+        <h1 className={styles.title}>Welcome to Virtu</h1>
       </main>
 
       <footer className={styles.footer}></footer>
